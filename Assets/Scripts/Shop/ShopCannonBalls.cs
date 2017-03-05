1aba421a13b6195dce638237728eb0890bc7ae83
raimis001 <raimis0001@gmail.com>
2017-03-05 13:53:16 +0200

    GUI shop buy balls and powder

---
+++ Assets/Scripts/Shop/ShopCannonBalls.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopCannonBalls : MonoBehaviour
+{
+
+	public Text textInShip;
+	public Text textBuy;
+
+	public int maxAmmo = 500;
+	public int currentAmmo = 60;
+	internal int selectAmmo = 0;
+
+	private Vector2 startDrag;
+
+	void Start()
+	{
+		textBuy.gameObject.SetActive(false);
+		textInShip.text = currentAmmo.ToString();
+	}
+
+	public void OnMouseDown()
+	{
+		if (currentAmmo >= maxAmmo) return;
+
+		textBuy.gameObject.SetActive(true);
+		startDrag = Input.mousePosition;
+		selectAmmo = 0;
+	}
+
+	public void OnMouseUp()
+	{
+		currentAmmo += selectAmmo;
+		textInShip.text = currentAmmo.ToString();
+
+		selectAmmo = 0;
+		textBuy.gameObject.SetActive(false);
+
+	}
+
+	public void OnMouseDrag()
+	{
+		if (currentAmmo >= maxAmmo) return;
+
+		float delta = startDrag.y - Input.mousePosition.y;
+
+		if (delta >= 0)
+		{
+			selectAmmo = 0;
+		}
+		else
+		{
+			delta = -delta;
+			float pd = delta/100f;
+			if (pd > 1) pd = 1;
+
+			selectAmmo = Mathf.CeilToInt(pd*(maxAmmo - currentAmmo));
+		}
+
+		textBuy.text = selectAmmo.ToString();
+	}
+}
