f09ae127dc104a4d5a75c8469037980ed639ab80
raimonds <raimis0001@gmail.com>
2017-03-09 19:24:30 +0200

    shop slot

---
+++ Assets/Scripts/Shop/ShopGoodResource.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopGoodResource : MonoBehaviour
+{
+
+	public static string SelectedResorce;
+
+	public GameObject selectedObject;
+
+	public EntityEconomy economy;
+	public string goodName;
+
+	private bool _selected;
+
+	public bool selected {
+		get { return _selected; }
+		set {
+			_selected = value;
+			selectedObject.SetActive(_selected);
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		selectedObject.SetActive(false);
+	}
+
+	public void OnMouseDown()
+	{
+		SelectedResorce = goodName;
+	}
+}
