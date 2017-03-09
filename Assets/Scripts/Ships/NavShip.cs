c4c49d3cc99ee528c884f10790feab1ad087dfb7
raimonds <raimis0001@gmail.com>
2017-03-09 17:32:34 +0200

    Nav mesh ships

--- Assets/Scripts/Ships/NavShip.cs
+++ Assets/Scripts/Ships/NavShip.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.AI;
 
-public class NavShip : MonoBehaviour
+public class NavShip : Ship
 {
 
 	public Transform target;
@@ -13,23 +13,61 @@ public class NavShip : MonoBehaviour
 	[Range(0, 1)]
 	public float frequency = 1;
 
+	[Header("Sunk")]
+	public float sunkSpeed = 2f;
 
-	private NavMeshAgent agent;
+	[Header("Params")]
+	public float damage;
+	public DamageManager damageManager;
+
+	protected NavMeshAgent agent;
 
 
 	// Use this for initialization
-	void Start()
+	protected virtual void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
 	}
 
 	// Update is called once per frame
-	void Update()
+	protected virtual void Update()
 	{
+		if (agent && agent.enabled)
+		{
+			agent.baseOffset += amplitude * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime)));
+			if (target)
+			{
+				agent.SetDestination(target.position);
+			}
+		}
+	}
 
-		agent.baseOffset += amplitude*(Mathf.Sin(2*Mathf.PI*frequency*Time.time) -Mathf.Sin(2*Mathf.PI*frequency*(Time.time - Time.deltaTime)));
-		agent.SetDestination(target.position);
+	public void SetHit(float hitpoint)
+	{
+		if (!agent || !agent.enabled) return;
 
+		damage += hitpoint;
+		if (damageManager) damageManager.damage = damage;
+		if (damage >= 1) Sunk();
+	}
+
+	public void Sunk()
+	{
+		if (!agent || !agent.enabled) return;
+
+		StartCoroutine(SunkShip());
+	}
+
+	IEnumerator SunkShip()
+	{
+		while (agent.baseOffset > -50)
+		{
+			agent.baseOffset -= sunkSpeed * Time.deltaTime;
+			yield return null;
+		}
+
+		agent.enabled = false;
+		Destroy(gameObject,0.5f);
 	}
 
 }
--- Assets/Scripts/Ships/PlayerShip.cs
+++ Assets/Scripts/Ships/PlayerShip.cs
@@ -1,39 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
-public class PlayerShip : Ship
+public class PlayerShip : NavShip
 {
 	[Header("Player")]
+	public float targetAngle = 0;
+
 	public float maxSpeed = 10;
 	public float throtle = 0.1f;
 	public float angularThrotle = 1f;
+
+	[Range(0, 50)]
+	public float angularSpeed;
+
+	[Range(0, 10)]
+	public float speed;
 
 	public WeaponManager weapons;
 
 	private float tAngle;
 
-	// Update is called once per frame
+	void Awake()
+	{
+		targetAngle = 90;
+	}
+
 	protected override void Update()
 	{
+		base.Update();
+
 		//Keyboard speed
 		speed += Input.GetKey(KeyCode.W) ? throtle : Input.GetKey(KeyCode.S) ? -throtle : 0;
 		speed = Mathf.Clamp(speed, 0, maxSpeed);
 
 		//Keyboard rotate
 		targetAngle += Input.GetKey(KeyCode.D) ? angularThrotle : Input.GetKey(KeyCode.A) ? -angularThrotle : 0;
+		float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, angularSpeed * Time.deltaTime) * Mathf.Deg2Rad;
 
-		//Do default move
-		base.Update();
+		if (agent && agent.enabled)
+		{
+			//Set move on nav mesh
+			agent.velocity = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * speed;
+		}
 
 		if (weapons && Input.GetKey(KeyCode.Space))
 		{
-			weapons.Shot(this);
+			//weapons.Shot(this);
 		}
-
 	}
 
 	public void OnCollisionStay(Collision collision)
 	{
-		//Debug.Log("Collision:" + collision.gameObject.tag);
-		if (collision.gameObject.CompareTag("Ground")) speed = 0;
 	}
 }
