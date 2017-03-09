c7792a3c2480391c4d463b450e944e1ab3938323
raimonds <raimis0001@gmail.com>
2017-03-09 19:08:51 +0200

    shop slot

--- Assets/Scripts/Shop/ShopGoodSlot.cs
+++ Assets/Scripts/Shop/ShopGoodSlot.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class ShopGoodSlot : ShopSlot
 {
+	public override bool SlotClick()
+	{
+		if (!base.SlotClick()) return false;
 
-	
+		Debug.Log("On good slot click");
+
+		return true;
+	}
 }
