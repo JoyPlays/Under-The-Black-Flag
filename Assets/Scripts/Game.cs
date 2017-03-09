665a26bf9175a4782b2c91c78ef6c751cb846fba
raimonds <raimis0001@gmail.com>
2017-03-09 18:58:06 +0200

    Game.cs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Game : MonoBehaviour
 {
 	public static Game Instance;
-	public List<City> cities;
+
+	internal List<City> cities;
 
 	void Awake()
 	{
 		Instance = this;
 		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start()
+	{
+		cities = GetComponentsInChildren<City>().ToList();
 	}
 }
