cf1306e0702af14eb2f5bac7fc43ae653bfa55d5
Davis <purelv@gmail.com>
2017-03-12 19:35:19 +0200

    Music on main menu

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public AudioSource backgroundMusic;
+	public AudioSource ambientShore;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
